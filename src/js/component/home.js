0137c5b89c95c1de6df9e0b1408122e9470d9f57
kevincastro015 <kevin.castro015@mymdc.net>
2021-07-08 00:38:50 +0000

    nameFieldOperating

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [render, setRender] = useState(true);
+	const [todo, setTodo] = useState("");
+	const [list, setList] = useState(["Mary", "Jane"]);
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<input
+				className="input"
+				placeholder="What needs to be done"
+				onChange={e => setTodo(e.target.value)}
+			/>
+			<button
+				className="btn btn-success"
+				onClick={() => {
+					setList([...list, todo]);
+					setTodo("");
+				}}>
+				Add
+			</button>
+			{list.map((element, index) => {
+				return <div key={index}>{element}</div>;
+			})}
 		</div>
 	);
 }
