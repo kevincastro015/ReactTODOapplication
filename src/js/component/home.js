b53232826cff2a0b4b1fb3ec9cab1f143df58ed9
kevincastro015 <kevin.castro015@mymdc.net>
2021-07-09 10:32:21 +0000

    deletebuttonAdded

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
-	const [render, setRender] = useState(true);
+	// const [render, setRender] = useState(true);
 	const [todo, setTodo] = useState("");
 	const [list, setList] = useState(["Mary", "Jane"]);
 
 	return (
-		<div className="text-center mt-5">
+		<div
+			className="text-center mt-5 bg-primary"
+			style={{ width: 400, height: 400 }}>
+			<h1>todos</h1>
 			<input
 				className="input"
 				placeholder="What needs to be done"
@@ -32,7 +35,9 @@ export function Home() {
 						<button
 							onClick={() =>
 								setList(list.filter(item => element !== item))
-							}></button>
+							}>
+							<span aria-hidden="true">&times;</span>
+						</button>
 					</div>
 				);
 			})}
