ffa05e18a54a5449754b1d88b833ceaf8f37bbe9
kevincastro015 <kevin.castro015@mymdc.net>
2021-07-09 20:04:30 +0000

    startedStyle

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -11,36 +11,47 @@ export function Home() {
 
 	return (
 		<div
-			className="text-center mt-5 bg-primary"
-			style={{ width: 400, height: 400 }}>
+			className="text-center mt-5 bg-light mx-auto p-4"
+			style={{ width: 400 }}>
 			<h1>todos</h1>
-			<input
-				className="input"
-				placeholder="What needs to be done"
-				onChange={e => setTodo(e.target.value)}
-				value={todo}
-			/>
-			<button
-				className="btn btn-success"
-				onClick={() => {
-					setList([...list, todo]);
-					setTodo("");
-				}}>
-				Add
-			</button>
-			{list.map((element, index) => {
-				return (
-					<div key={index}>
-						{element}
-						<button
-							onClick={() =>
-								setList(list.filter(item => element !== item))
-							}>
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-				);
-			})}
+			<div className="border">
+				<input
+					className="input"
+					placeholder="What needs to be done"
+					onChange={e => setTodo(e.target.value)}
+					value={todo}
+				/>
+				<button
+					className="btn btn-success"
+					onClick={() => {
+						setList([...list, todo]);
+						setTodo("");
+					}}>
+					Add
+				</button>
+				{list.map((element, index) => {
+					return (
+						<div className="border-top border-bottom" key={index}>
+							{element}
+							<button
+								onClick={() =>
+									setList(
+										list.filter(item => element !== item)
+									)
+								}>
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+					);
+				})}
+				<div className="counter">Item(s) Left {list.length}</div>
+			</div>
+			<div
+				className="border mx-auto"
+				style={{ width: "98%", height: 3 }}></div>
+			<div
+				className="border mx-auto"
+				style={{ width: "96%", height: 3 }}></div>
 		</div>
 	);
 }
